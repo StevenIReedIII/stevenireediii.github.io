ebe4f477a66b7a149e65b1344a4385bb51db5d08
Steven Reed <101155741+StevenIReedIII@users.noreply.github.com>
2024-10-04 09:04:55 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -100,18 +100,11 @@ const CONFIG = {
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
-    },
-    {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'BrockISD Technology',
+      position: 'Intern',
+      from: 'June 2023',
+      to: 'December 2023',
+      companyLink: 'https://brockisd.net',
     },
   ],
   certifications: [
